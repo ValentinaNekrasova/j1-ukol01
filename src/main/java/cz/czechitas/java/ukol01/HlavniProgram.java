fbb11def95e1fe74f21bc5a8e6ee74198ad40443
Valentina <valentinanekrasova@seznam.cz>
2023-02-19 20:36:10 +0100

    Prasatko

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -8,6 +8,51 @@ public class HlavniProgram {
         Turtle zofka = new Turtle();
 
         //TODO implementace domácího úkolu
+
+        nakresliPrasatko(zofka);
+
+        zofka.setLocation(80,80);
+    }
+
+    private static void nakresliPrasatko(Turtle zofka) {
+        nakresliObdelnik(zofka);
+
+        nakresliNozicky(zofka);
+
+        zofka.turnLeft(135);
+        zofka.move(200);
+        zofka.turnRight(90);
+
+        nakresliNozicky(zofka);
+
+        nakresliTrojuhelnik(zofka);
+    }
+
+    private static void nakresliTrojuhelnik(Turtle zofka) {
+        zofka.turnLeft(90);
+        zofka.move(71);
+        zofka.turnRight(90);
+        zofka.move(71);
+    }
+
+    private static void nakresliNozicky(Turtle zofka) {
+        zofka.turnRight(135);
+        zofka.move(30);
+        zofka.turnRight(180);
+        zofka.move(30);
+        zofka.turnLeft(90);
+        zofka.move(30);
+        zofka.turnRight(180);
+        zofka.move(30);
+    }
+
+    private static void nakresliObdelnik(Turtle zofka) {
+        for (int i = 0; i < 2; i++) {
+            zofka.move(100);
+            zofka. turnLeft(90);
+            zofka. move(200);
+            zofka.turnLeft(90);
+        }
     }
 
     public static void main(String[] args) {
