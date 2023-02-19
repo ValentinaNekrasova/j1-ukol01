6b1cc6ac980030b2a63d9d53d6a08ef278aef37d
Valentina <valentinanekrasova@seznam.cz>
2023-02-19 22:15:56 +0100

    CelyUkol

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -9,18 +9,69 @@ public class HlavniProgram {
 
         //TODO implementace domácího úkolu
 
-        zofka.setLocation(600,350);
 
-        nakresliPrasatko(zofka);
-
-        zofka.setLocation(85,85);
+        zofka.setLocation(80,90);
 
         nakresliSlunicko(zofka);
 
-        zofka.setLocation(500,200);
+        zofka.setLocation(30,210);
 
+        for (int i = 0; i < 6; i++) {
+            zofka.penDown();
+            nakresliDomecek(zofka);
+            zofka.penUp();
+            zofka.turnLeft(45);
+            zofka.move(80);
+            zofka.turnLeft(90);
+        }
 
+        zofka.setLocation(600,470);
+        zofka.penDown();
 
+        nakresliPrasatko(zofka);
+
+        zofka.setLocation(800,430);
+
+        zofka.turnRight(115);
+        zofka.move(80);
+        zofka.turnLeft(140);
+        zofka.move(80);
+
+        zofka.penUp();
+        zofka.setLocation(zofka.getX()+30, zofka.getY()+75);
+        zofka.turnLeft(20);
+        zofka.penDown();
+        zofka.move(75);
+        zofka.turnRight(145);
+        zofka.move(90);
+        zofka.turnLeft(145);
+        zofka.move(73);
+
+        zofka.setLocation(zofka.getX()+40, zofka.getY()+40);
+
+        nakresliKyticku(zofka);
+    }
+
+    private static void nakresliKyticku(Turtle zofka) {
+        for (int i = 0; i < 11; i++) {
+            zofka.move(5);
+            zofka.turnLeft(90);
+            zofka.move(25);
+            zofka.turnRight(180);
+            zofka.move(25);
+            zofka.turnRight(8);
+        }
+    }
+
+    private static void nakresliDomecek(Turtle zofka) {
+        for (int i = 0; i < 4; i++) {
+            zofka.turnRight(90);
+            zofka.move(100);
+        }
+        zofka.turnRight(45);
+        zofka.move(70);
+        zofka.turnRight(90);
+        zofka.move(70);
     }
 
     private static void nakresliSlunicko(Turtle zofka) {
